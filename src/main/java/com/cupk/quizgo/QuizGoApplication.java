125d64081988839a39127f2ecbaa0fce508ae66e
Linsen HU <1563883475@qq.com>
2026-04-21 12:00:57 +0800

    Initial commit

---
+++ src/main/java/com/cupk/quizgo/QuizGoApplication.java
@@ -0,0 +1,13 @@
+package com.cupk.quizgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cupk/quizgo/QuizGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupk.quizgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
