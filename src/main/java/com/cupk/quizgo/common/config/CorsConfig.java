15099e8b79fe467bcb1ec2d1a93bc12c417aa2e1
Linsen HU <1563883475@qq.com>
2026-04-22 12:43:29 +0800

    Create CorsConfig.java

---
+++ src/main/java/com/cupk/quizgo/common/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.cupk.quizgo.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        // 允许前端8080端口跨域
+        config.addAllowedOrigin("http://localhost:8080");
+        // 允许携带cookie
+        config.setAllowCredentials(true);
+        // 允许所有请求头
+        config.addAllowedHeader("*");
+        // 允许所有请求方法
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
